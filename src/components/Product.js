feffd508094945e155064357f15c5401ac30409b
github username <github email address>
2017-08-28 21:33:48 -0400

    Done.

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,1 +1,53 @@
 // Code Product Component Here
+
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export default class Product extends React.Component {
+  render() {
+    return (
+    <div>
+      <ul>
+        <li>{this.props.name}</li>
+        <li>{this.props.producer}</li>
+        <li>{this.props.hasWatermark}</li>
+        <li>{this.props.color}</li>
+        <li>{this.props.weight}</li>
+      </ul>
+    </div>
+    )
+  }
+}
+
+Product.defaultProps = {
+  hasWatermark: false
+};
+
+Product.propTypes = {
+  name: PropTypes.string.isRequired,
+  producer: PropTypes.string,
+  hasWatermark: PropTypes.bool,
+  color: PropTypes.oneOf(['white', 'eggshell-white', 'salmon']).isRequired,
+  weight: (props, propName) => {
+    let weight = props[propName]
+    if (!(weight > 80 && weight < 300)) {
+      return new Error('The `weight` prop should range between 80 and 300.');
+    }
+    if (weight === undefined) {
+      return new Error('The `weight` prop is required');
+    }
+    if (isNaN(weight)) {
+      return new Error('The `weight` prop is not a number.');
+    }
+  }
+};
+
+// Order.propTypes = {
+//   cone: PropTypes.bool,
+//   size: PropTypes.string,
+//   scoops: PropTypes.arrayOf(PropTypes.string).isRequired,
+//   orderInfo: PropTypes.shape({
+//     customerName: PropTypes.string.isRequired,
+//     orderedAt: PropTypes.number.isRequired // We're using UNIX timestamps here
+//   }).isRequired
+// };
